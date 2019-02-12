5d827ca66c8be0da8d6cc4a4db4708246ec90e2f
WiFuchs <willfuchs13@gmail.com>
2019-02-12 11:35:20 -0800

    Add test streaming from Google Drive

--- src/App.js
+++ src/App.js
@@ -50,8 +50,16 @@ class App extends Component {
       <div className="App">
         <HeaderBar />
         <PodCat podcasts={this.state.podcasts} />
-        <PodCat podcasts={this.state.podcasts} />
-        <PodCat podcasts={this.state.podcasts} />
+        <iframe
+          src="https://drive.google.com/drive/folders/16eSyuz-mgDVpGo_7sL65jaK8v-e_vMYN?usp=sharing"
+          width="500"
+          title="testVis"
+          height="30"
+          frameborder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowfullscreen
+        />
         <Footer />
       </div>
     );
